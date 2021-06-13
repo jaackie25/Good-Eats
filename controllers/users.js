560ed32fb940ca4f26fb98956f10f08cc5bda2c6
jaackie25 <jackiedinh91@gmail.com>
2021-06-12 21:35:04 -0700

    added user fav functionality

--- controllers/users.js
+++ controllers/users.js
@@ -6,16 +6,29 @@ const axios = require('axios')
 
 // GET -- Favorites button on nav to view favorites
 router.post('/favorites/:id', (req, res) => {
+   console.log(req.body)
+    console.log(req.params)
     axios.get(`http://www.themealdb.com/api/json/v1/1/lookup.php?i=${req.params.id}`)
     .then(resFav => {
         const meals = resFav.data.meals
         JSON.stringify(meals)
-        console.log(resFav.data.meals[0].strMeal)
+        db.user.findOne({
+            where: {
+                email: req.body.useremail
+            }
+        }). then (user => {
+            db.recipe.create({
+                name: req.body.recipe,
+                userId: user.dataValues.id
+            })
+            console.log(user.dataValues.id)
+        })
         // db.recipe.create({
         //     name: 
         // })
+        res.render('users/index.ejs', {meals})
     })
-    res.render('users/index.ejs')
+   
 })
 
 // post to grab hidden form to add favorites (button on recipes id page) -- redirect to favorites page
